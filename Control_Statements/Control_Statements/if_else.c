66c1251c9ec5e2e578745e9871d15af099aa134b
vishusharma <sikaridon9@gmail.com>
2025-10-09 20:22:58 +0530

    if_else

---
+++ Control_Statements/Control_Statements/if_else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // if-else-if ladder
+    if (number > 0) {
+        printf("The number is positive.\n");
+    } else if (number < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
